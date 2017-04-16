10d2cac7e34334d8befa8bc3f3e69437f14b9c85
Sean Yesmunt <sean.yesmunt@gmail.com>
2017-04-15 19:48:37 -0700

    change to 5000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const bodyParser = require('body-parser')
 const axios = require('axios')
 const morgan = require('morgan')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
